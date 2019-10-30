1c63affd570464577db99b321e468ab7bcc220e2
DatLe <datle1108@gmail.com>
2019-10-30 21:27:42 +0700

    Add validate ui for form

--- client/src/components/stream/StreamForm.js
+++ client/src/components/stream/StreamForm.js
@@ -8,22 +8,37 @@ class StreamForm extends React.Component {
         this.props.onSubmit(formValues);
     }
 
-    renderInput({input}) {
-        return <input {...input}/>
+    renderError = ({ touched, error }) => {
+
+        if (touched != false && error) {
+            return (
+                <div className="ui error message">
+                    <p>{error}</p>
+                </div>
+            );
+        } else {
+            return null;
+        }
+        
+    }
+
+    renderInput = ({ input, meta, label}) => {
+        const className = `field ${meta.error ? 'error' : ''}`;
+        return (
+            <div className={className}>
+                <label>{label}</label>
+                <input {...input}/>
+                {this.renderError(meta)}
+            </div>
+        );
     }
 
     render() {
         return (
             <div>
-                <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmitButtonClicked)}>
-                    <div className="field">
-                        <label htmlFor="streamName">Stream name</label>
-                        <Field name="name" component={this.renderInput} type="text"/>
-                    </div>
-                    <div className="field">
-                        <label htmlFor="streamDescription">Stream description</label>
-                        <Field name="description" component={this.renderInput} type="text"/>
-                    </div>
+                <form className="ui form error" onSubmit={this.props.handleSubmit(this.onSubmitButtonClicked)}>
+                    <Field name="name" label="Stream name" component={this.renderInput}/>
+                    <Field name="description" label="Stream Description" component={this.renderInput}/>
                     <Link className="ui button right floated" to="/">Cancel</Link>
                     <button className="ui button primary right floated" type="submit">Submit</button>
                 </form>
@@ -35,13 +50,13 @@ const validate = (formValue) => {
     const errors = {};
 
     if (!formValue.name) {
-        errors.title = 'Please enter stream name';
+        errors.name = 'Please enter stream name';
     }
 
     if (!formValue.description) {
         errors.description = 'Please enter stream description';
     }
-    console.log(errors);
+
     return errors;
 };
 
